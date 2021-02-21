7ad5df86c7522296584af5fb40314a99b8aa4c33
unknown <sandy@ssagroup.com>
2021-02-21 11:08:42 +0800

    Add function for Avatar and Fullname

--- app/User.php
+++ app/User.php
@@ -2,22 +2,22 @@
 
 namespace App;
 
+use App\Enumerations\NameStandard as Names;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravolt\Avatar\Avatar;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     use Notifiable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are not mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -27,13 +27,44 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    // Line 38 and 48 is getting for fake avatar.
+    
+    /**
+     * Retrieve the Avatar instance.
+     * 
+     * @param  array|null $config
+     * @return \Laravolt\Avatar\Avatar
+     */
+    protected function laravolt($config = null)
+    {
+    	return new Avatar($config ?? config('avatar'));
+    }
 
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
+     * Retrieve the url of the user's avatar.
+     * 
+     * @return string
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getAvatarAttribute()
+    {
+    	return $this->photo ?? $this->laravolt()
+    		->create($this->fullname)
+    		->toBase64()
+    		->getEncoded();
+    }
+
+    /**
+     * Retrieve the global display name.
+     * 
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+    	return strtr(Names::FULLNAME, [
+    		Names::FIRSTNAME => $this->firstname,
+    		Names::MIDDLEINITIAL => str_limit($this->middlename, 1, '.'),
+    		Names::LASTNAME => $this->lastname,
+    	]);
+    }
 }
